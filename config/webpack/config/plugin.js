84422d1d3f5926c97d8aa61f79a00b27d6181afc
chenzhuonan <chenzhuonan@yy.com>
2020-11-10 08:03:53 +0800

    add-plugin

--- config/webpack/config/plugin.js
+++ config/webpack/config/plugin.js
@@ -1,8 +1,27 @@
+const webpack = require('webpack')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 modules.export = (env,config)=>{
-  
+
   const pluginConfig = {
     plugin:{
-      env
+      env:{
+        plugin: webpack.EnvironmentPlugin,
+        args:[{
+          NODE_ENV:env
+        }]
+      },
+      clean:{
+        plugin:CleanWebpackPlugin
+      },
+      html:{
+        plugin:HtmlWebpackPlugin,
+        args:[{
+          title:'Page',
+          template:
+        }]
+      }
     }
   }
 
